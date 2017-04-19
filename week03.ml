58880d96a6238cde8a05bfd196a0b10ed3467e36
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-04-19 08:40:50 -0700

    added solutions

--- week03.ml
+++ week03.ml
@@ -13,38 +13,56 @@ type 'a option = None
 *)
 
 let is_none (opt: 'a option) : bool =
-  error "undefined"
+  match opt with
+  | None   -> true
+  | Some _ -> false
 
 let option_map (f: 'a -> 'b) (opt: 'a option) : 'b option =
-  error "undefined"
+  match opt with
+  | None   -> None
+  | Some a -> Some (f a)
 
 let option_app (optf: ('a -> 'b) option) (opt: 'a option) : 'b option =
-  error "undefined"
+  match optf with
+  | None   -> None
+  | Some f -> option_map f opt
         
 let option_default (def: 'a) (opt: 'a option) : 'a =
-  error "undefined"
+  match opt with
+  | None   -> def
+  | Some a -> a
 
 let option_get (opt: 'a option) : 'a =
-  error "undefined"
+  match opt with
+  | None   -> error "opt is None!"
+  | Some a -> a
 
-let rec find (ls: ('a * 'b) list) (x: 'a) : 'b option =
-  error "undefined"
+let rec find (ls: ('a * 'b) list) (key: 'a) : 'b option =
+  match ls with
+  | []       -> None
+  | (a,b)::t -> if a = key then Some b else find t key
 
 (* infix functions: 
    https://caml.inria.fr/pub/docs/manual-caml-light/node4.9.html
  *)
 
 let res0 = 
-  let helper (op: 'a -> 'b -> 'c) (x: 'a option) (y: 'b option) : 'c option = 
-    error "undefined" 
+  let helper (f: 'a -> 'b -> 'c) (x: 'a option) (y: 'b option) : 'c option = 
+    match (x,y) with
+    | (None,_) -> None
+    | (_,None) -> None
+    | (Some a, Some b) -> Some (f a b)
   in
   let ( =/ ) (x: int option) (y: int option) : int option = 
-    error "undefined" 
+    match (x,y) with
+    | (None,_) -> None
+    | (_,None) -> None
+    | (Some a, Some b) -> if b = 0 then None else Some (a / b)
   in
   let ( =* ) = helper ( * ) in
   let ( =+ ) = helper ( + ) in
   let ( =- ) = helper ( - ) in
-  (Some 3 =/ Some 2) =* (Some 1 =- Some 3) =+ (Some 5)
+  (Some 3 =/ Some 2) =* (None =- Some 3) =+ (Some 5)
 
 
 
@@ -55,10 +73,14 @@ let isEven x =
   x mod 2 = 0
 
 let rec evens (l: int list) : int list =
-  error "undefined"
+  match l with
+  | [] -> []
+  | h::t -> if isEven h then h :: (evens t) else evens t
 
 let rec fourLetters (l: string list) : string list =
-  error "undefined"
+  match l with
+  | [] -> []
+  | h::t -> if String.length h = 4 then h :: (fourLetters t) else fourLetters t
 
 
 
@@ -71,11 +93,13 @@ let rec fourLetters (l: string list) : string list =
 
 
 let rec filter (pred: 'a -> bool) (l: 'a list) : 'a list =
-  error "undefined"
+  match l with
+  | [] -> []
+  | h::t -> if pred h then h :: (filter pred t) else filter pred t
 
-let evens'       = error "undefined"
+let evens'       = filter isEven
 
-let fourLetters' = error "undefined"
+let fourLetters' = filter (fun x -> String.length x = 4)
 
 
 
@@ -84,10 +108,15 @@ let fourLetters' = error "undefined"
 
 
 let rec listUpper (l: string list) : string list =
-  error "undefined"
+  match l with
+  | []   -> []
+  | h::t -> (String.uppercase h) :: (listUpper t)
+  
 
 let rec listSquare (l: int list) : int list =
-  error "undefined"
+  match l with
+  | []   -> []
+  | h::t -> (h * h) :: (listSquare t)
  
 
 
@@ -100,12 +129,14 @@ let rec listSquare (l: int list) : int list =
 
  
 let rec map (f: 'a -> 'b) (l: 'a list) : 'b list =
-  error "undefined"
+  match l with
+  | []   -> []
+  | h::t -> f h :: map f t
 
 
-let listUpper'  = error "undefined" 
+let listUpper'  = map String.uppercase
 
-let listSquare' = error "undefined"
+let listSquare' = map (fun x -> x * x)
 
 
 
@@ -113,10 +144,14 @@ let listSquare' = error "undefined"
 (* Fold ******************************************)
 
 let rec sum (l: int list) : int = 
-  error "undefined"
+  match l with
+  | []   -> 0
+  | h::t -> h + sum t
 
 let rec concat (l: string list) : string = 
-  error "undefined"
+  match l with
+  | []   -> ""
+  | h::t -> h ^ concat t
 
 
 
@@ -129,13 +164,22 @@ let rec concat (l: string list) : string =
 
 
 let rec fold_right (step: 'a -> 'acc -> 'acc) (l: 'a list) (acc: 'acc) : 'acc =
-  error "undefined"
+  match l with
+  | []   -> acc
+  | h::t -> step h (fold_right step t acc)
 
 
+let sum' l  = fold_right (+) l 0
 
-let sum'    = error "undefined"
+(*
+sum' [1;2;3] = fold_right (+) [1;2;3] 0
+             = (+) 1 (fold_right (+) [2;3] 0)
+             = (+) 1 (2 (+) (fold_right (+) [3] 0))
+             = (+) 1 (2 (+) (3 (+) (fold_right (+) [] 0)))
+             = 1 (+) (2 (+) (3 (+) 0))
+ *)
 
-let concat' = error "undefined"
+let concat' l = fold_right (^) l ""
 
 
 
@@ -143,10 +187,20 @@ let concat' = error "undefined"
 (* Fold (Tail Recursive) ******************************************)
 
 let rec sum_tr (l: int list) : int = 
-  error "undefined"
+  let rec helper l' acc = 
+    match l' with
+    | []   -> acc
+    | h::t -> helper t (h + acc)
+  in
+  helper l 0
 
 let rec concat_tr (l: string list) : string = 
-  error "undefined"
+  let rec helper l' acc = 
+    match l' with
+    | []   -> acc
+    | h::t -> helper t (h ^ acc)
+  in
+  helper l ""
 
 
 
@@ -159,15 +213,28 @@ let rec concat_tr (l: string list) : string =
 
 
 let rec fold_left (step: 'acc -> 'a -> 'acc) (acc: 'acc) (l: 'a list) : 'acc =
-  error "undefined"
-
+  let rec helper l' acc = 
+    match l' with
+    | []   -> acc
+    | h::t -> helper t (step acc h)
+  in
+  helper l acc
 
 
 
 (* Fold examples ******************************************)
 
 let filter' (pred: 'a -> bool) (l: 'a list) : 'a list =
-  error "undefined"
+  let step acc a = if pred a then acc @ [a] else acc in
+  let acc = [] in
+  fold_left step acc l
+
 
 let map' (f: 'a -> 'b) (l: 'a list) : 'b list =
-  error "undefined"
+  let step a acc = f a :: acc in
+  let acc = [] in
+  fold_right step l acc
+
+let f1 = filter' isEven [1;2;3;4;5]
+
+let f2 = map' String.uppercase ["foo"; "bar"]
